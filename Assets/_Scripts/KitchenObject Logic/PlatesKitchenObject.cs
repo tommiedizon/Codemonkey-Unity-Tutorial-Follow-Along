4d38453c16f5e15fe8a85e202c6ec81a559745ff
Thomas Dizon <thomas.dizon25@gmail.com>
2025-01-22 10:49:35 +1100

    added ability for plateKitchenObject to detect valid kitchenObjects that can be placed on it. Demo: https://drive.google.com/file/d/1pO6ddhWZwP-lITtInQP_SNev8kXFYtT5/view?usp=drive_link

--- Assets/_Scripts/KitchenObject Logic/PlatesKitchenObject.cs
+++ Assets/_Scripts/KitchenObject Logic/PlatesKitchenObject.cs
@@ -7,10 +7,18 @@ public class PlatesKitchenObject : KitchenObject
 
     private List<KitchenObjectSO> kitchenObjectSOList;
 
+    [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
+
     private void Awake() {
         kitchenObjectSOList = new List<KitchenObjectSO>();
     }
     public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO) {
+
+        if (!validKitchenObjectSOList.Contains(kitchenObjectSO)) {
+            // Not a valid KitchenObject
+            return false;
+        }
+
         if (kitchenObjectSOList.Contains(kitchenObjectSO)) {
             // already has this kitchenObject
             return false;
